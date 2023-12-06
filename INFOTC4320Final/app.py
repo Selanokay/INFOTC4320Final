5781b7d402d7dee73898f19af0919f2619b463cc
Selanokay <83822255+Selanokay@users.noreply.github.com>
2023-12-06 17:17:39 -0600

    Update app.py
    Updates made by Peter to app.py to hopefully allow for checking of passcodes.txt further down the line. also updated function names

--- INFOTC4320Final/app.py
+++ INFOTC4320Final/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name)
+@app.route('/')
+def main():
+    return render_template('Main.html')
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/Admin')
+def admin():
+    return render_template('Admin.html')
+
+@app.route('/Admin', methods=('GET', 'POST'))
+def check_admin():
+        username = request.form['userUsername']
+        password = request.form['userPassword']
+        if not username:
+            flash('Username is required!')
+        elif not password:
+            flash('Password is required')
+        else:
+            if (username in admin_credentials and admin_credentials[username] == password):
+                return redirect(url_for('admin_info'))
+            else:
+                flash("Invalid Username or Password.")
+        return render_template("Admin.html")
+
+
+@app.route('/Reservation')
+def reservation():
+    return render_template('Reservation.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
